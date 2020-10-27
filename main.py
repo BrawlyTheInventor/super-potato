8c425d6cefbde6e9aee6a28479ebf6286e5876cf
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-10-27 14:25:05 -0400

    Fixed glitch - potato not moving to full frame

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ def left():
 
 print("It's a " + choice(["potato", "tomato", "chicken", "SUPER POTATO"]))
 penup()
-goto(randint(1, 100), randint(1, 100))
+goto(randint(1, 10000000), randint(1, 10000000))
 screen = Screen()
 screen.setup(800, 560)
 screen.setworldcoordinates(0, 0, 800, 560)
 screen.listen()
-goto(randint(1, 100), randint(1, 100))
 screen.onkey(forward, 'w')
 screen.onkey(backward, 's')
 screen.onkey(right, 'a')
 screen.onkey(left, 'd')
+while True:
+	penup()
+	goto(randint(1, 100), randint(1, 100))
+
