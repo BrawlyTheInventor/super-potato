4616af70024dc6bc25220c93897e427363ee0bb2
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-10-27 10:35:59 -0400

    Added player

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from random import *
 from turtle import *
+from varables import *
+player = Turtle()
+player.penup()
+def forward():
+	player.forward(customforward)
+def backward():
+	player.backward(custombackward)
+def right():
+	player.right(customright)
+def left():
+	player.left(customleft)
+
 print("It's a " + choice(["potato", "tomato", "chicken", "SUPER POTATO"])
 penup()
 goto(randint(1, 100), randint(1, 100))
-print("YOUR TURN")
-playerturn = input(">>>")
-
-if playerturn == "End":
-			print("Come back later")
+screen = Screen()
+screen.setup(800, 560)
+screen.setworldcoordinates(0, 0, 800, 560)
+screen.listen()
+goto(randint(1, 100), randint(1, 100))
+screen.onkey(forward, 'w')
+screen.onkey(backward, 's')
+screen.onkey(right, 'a')
+screen.onkey(left, 'd')
