911a125b032d3a8a8705c456d3b1c2ec71c428d6
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-10-27 14:37:29 -0400

    added dynamic food.

--- main.py
+++ main.py
@@ -11,8 +11,36 @@ def right():
 	player.right(customright)
 def left():
 	player.left(customleft)
-
-print("It's a " + choice(["potato", "tomato", "chicken", "SUPER POTATO"]))
+type = choice(["potato", "tomato", "chicken", "SUPER POTATO"])
+print("It's a " + type)
+if type == "tomato":
+	# registering the image 
+	# as a new shape 
+	register_shape('tomato.gif') 
+  
+# setting the image as cursor 
+	shape('tomato.gif') 
+elif type == "potato":
+	# registering the image 
+# as a new shape 
+	register_shape('potato.gif') 
+  
+# setting the image as cursor 
+	shape('potato.gif') 
+elif type == "chicken":
+	# registering the image 
+# as a new shape 
+	register_shape('chicken.gif') 
+elif type == "SUPER POTATO":
+	# registering the image 
+# as a new shape 
+	register_shape('superpotato.gif') 
+  
+# setting the image as cursor 
+	shape('superpotato.gif') 
+  
+# setting the image as cursor 
+	shape('chicken.gif') 
 penup()
 goto(randint(1, 10000000), randint(1, 10000000))
 screen = Screen()
