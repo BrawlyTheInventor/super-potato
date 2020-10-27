fff932c2738404cd3116e43238e2a4433ce5e39e
BrawlyTheInventor <49785023+Unkown2@users.noreply.github.com>
2020-10-26 22:57:23 -0400

    Added SUPER POTATO graphics

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from random import *
+from turtle import *
 print("It's a " + choice(["potato", "tomato", "chicken", "SUPER POTATO"])
+penup()
+goto(randint(1, 100), randint(1, 100))
+print("YOUR TURN")
+playerturn = input(">>>")
