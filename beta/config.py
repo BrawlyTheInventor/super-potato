5d0b8fbe496b27a8f530d20eb6f88220d4ad05fe
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-10-29 13:06:14 -0400

    Update config.py

--- beta/config.py
+++ beta/config.py
@@ -3,3 +3,7 @@ enable-beta-mode = True
 enable-multiplayer = True
 addons-enabled = True
 #Addons are scripts that are run on your computer. They can change the gameplay, make custom servers, and more!
+customforward = 100
+custombackward = 100
+customright = 100
+customleft = 100
