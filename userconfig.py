6e036acfbf27210c3b1e5c1ecedeac0a0f75c562
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-10-29 13:05:01 -0400

    Delete userconfig.py

--- userconfig.py
+++
@@ -1,4 +0,0 @@
-enable-beta-mode = True
-enable-multiplayer = True
-addons-enabled = True
-#Addons are scripts that are run on your computer. They can change the gameplay, make custom servers, and more!
